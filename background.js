1d1a2a758b3dd2941b1d43c8605d38c27fc0ebcb
jasonleyser <jason.leyser@protocol.ai>
2021-02-08 11:54:47 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 //When the Slate icon is clicked, execute the App script
 chrome.browserAction.onClicked.addListener(function(tabs) {
-  alert('hello')
   console.log(tabs)
   //inject all scripts needed
   chrome.tabs.executeScript({file: "./app/core/jquery.min.js"});
@@ -8,12 +7,10 @@ chrome.browserAction.onClicked.addListener(function(tabs) {
   chrome.tabs.executeScript({file: "./app/core/app.js"});
   chrome.tabs.executeScript({file: "./app/core/scripts.js"});
   chrome.tabs.executeScript({file: "./app/core/page-data.js"});
-
   //send message to backgorund that button was clicked
   chrome.tabs.query({active: true, currentWindow:true},
     function(tabs) {
      var activeTab = tabs[0];
-
      chrome.tabs.sendMessage(activeTab.id,
          {"message": "clicked_browser_action"}
      );
