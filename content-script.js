3de88b6151678c9e92e580735b26f64d34fefa81
jasonleyser <jason.leyser@protocol.ai>
2021-02-16 10:26:21 -0700

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
   //Wait for message from 'background.js'
   //When message is recieved, inject the Slate App into the current tab
   chrome.runtime.onMessage.addListener(
+
     async function(request, callback) {
       if (request.message == "clicked_browser_action"){
         let allPageFiles = await GetPageFiles();
-
         var appMain = document.getElementById('slate-app')
-        var changeThem = document.getElementById("slate-image-grid")
+        var imageGrid = document.getElementById("slate-image-grid")
         var img_array = []
-        //Required for arrays in Chrome Extensions
+        //Required format for arrays in Chrome Extensions
         img_array = Array.from(allPageFiles);
-        var listThem = ListFiles(img_array)
+        var imageGrid = ListFiles(img_array)
         appMain.style.display = 'inline'
+
       }
     }
   );
