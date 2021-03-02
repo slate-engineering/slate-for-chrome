94b41e477fe032676804747e16551db16c2f022d
jasonleyser <jason.leyser@protocol.ai>
2021-03-01 20:09:32 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,30 +1,34 @@
-const InsertJquery = () => {
-  console.log('jquery inserted');
-}
-
-const LoadApp = () => {
-  console.log('sending message')
+LoadApp = () => {
   //Send message to app/index.js that Slate is loaded
-  chrome.tabs.query({ active: true, currentWindow:true },
-    function(tabs) {
-       var activeTab = tabs[0];
-       chrome.tabs.sendMessage(activeTab.id,
-           {"message": "clicked_browser_action"}
-       );
-   });
-   console.log('message sent')
-}
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {
+      message: "openSlateApp",
+    });
+  });
+};
 
 chrome.runtime.onInstalled.addListener(function (tab) {
-  //on new install, open the settings page
-  chrome.tabs.create({url: chrome.extension.getURL('app/pages/settings.html')});
+  //on new install, open the welcome page
+  chrome.tabs.create({
+    url: chrome.extension.getURL("app/pages/welcome.html"),
+  });
 });
 
 //Wait for Slate Extension icon to be clicked
-chrome.browserAction.onClicked.addListener(function(tabs) {
-  console.log(tabs)
+chrome.browserAction.onClicked.addListener(function (tabs) {
   //inject all Slate scripts needed into the current tab
-  var activeTab = tabs[0];
-  chrome.tabs.executeScript(activeTab, {file: "app/scripts/jquery.min.js"}, InsertJquery());
-  chrome.tabs.executeScript(activeTab, {file: "content-script.js"}, LoadApp());
+  let activeTab = tabs[0];
+  chrome.tabs.executeScript(activeTab, { file: "app/scripts/jquery.min.js" });
+  chrome.tabs.executeScript(
+    activeTab,
+    { file: "content-script.js" },
+    LoadApp()
+  );
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.uploadData == "slate") {
+    alert("im in the script");
+  }
 });
